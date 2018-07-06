8e55f3c6605a7948ffd6c4d5170b077d75739718
Jean-Baptiste Pionnier <jb.pionnier@gmail.com>
2018-07-06 16:40:45 +0200

    feature: add status response

--- lib/explorers/api-response.explorer.ts
+++ lib/explorers/api-response.explorer.ts
@@ -1,94 +1,44 @@
-import { isFunction } from '@nestjs/common/utils/shared.utils';
-import { mapValues, omit } from 'lodash';
+import { omit } from 'lodash';
 import { DECORATORS } from '../constants';
-import {
-  exploreModelDefinition,
-  mapTypesToSwaggerTypes
-} from './api-parameters.explorer';
 
-export const exploreGlobalApiResponseMetadata = (definitions, metatype) => {
-  const responses = Reflect.getMetadata(DECORATORS.API_RESPONSE, metatype);
-  return responses
-    ? {
-        responses: mapResponsesToSwaggerResponses(responses, definitions)
-      }
-    : undefined;
+const initialMetadata = {
+  status: 0,
+  type: String,
+  isArray: false
 };
 
-export const exploreApiResponseMetadata = (
-  definitions,
-  instance,
-  prototype,
-  method
-) => {
-  const responses = Reflect.getMetadata(DECORATORS.API_RESPONSE, method);
-  if (!responses) {
-    return undefined;
-  }
-  return mapResponsesToSwaggerResponses(responses, definitions);
-};
-
-const omitParamType = param => omit(param, 'type');
-
-const mapResponsesToSwaggerResponses = (responses, definitions) =>
-  mapValues(
-    mapValues(responses, response => {
-      const { type, isArray } = response;
-      response = omit(response, ['isArray']);
-
-      if (!type) {
-        return response;
-      }
-      const defaultTypes = [String, Boolean, Number, Object, Array];
-      if (
-        !(
-          isFunction(type) &&
-          !defaultTypes.find(defaultType => defaultType === type)
-        )
-      ) {
-        const metatype: string = type && isFunction(type) ? type.name : type;
-        const swaggerType = mapTypesToSwaggerTypes(metatype);
-
-        if (isArray) {
-          return {
-            ...response,
-            schema: {
-              type: 'array',
-              items: {
-                type: swaggerType
-              }
-            }
-          };
-        }
-        return {
-          ...response,
-          schema: {
-            type: swaggerType
-          }
-        };
-      }
-      const name = exploreModelDefinition(type, definitions);
-      if (isArray) {
-        return toArrayResponseWithDefinition(response, name);
-      }
-      return toResponseWithDefinition(response, name);
-    }),
-    omitParamType
-  );
-
-export const toArrayResponseWithDefinition = (response, name) => ({
-  ...response,
-  schema: {
-    type: 'array',
-    items: {
-      $ref: `#/definitions/${name}`
+export const ApiResponse = (metadata: {
+  status: number;
+  description?: string;
+  type?: any;
+  isArray?: boolean;
+}) => {
+  metadata.description = metadata.description ? metadata.description : '';
+  const groupedMetadata = { [metadata.status]: omit(metadata, 'status') };
+  return (target, key?, descriptor?: PropertyDescriptor) => {
+    if (descriptor) {
+      const responses =
+        Reflect.getMetadata(DECORATORS.API_RESPONSE, descriptor.value) || {};
+      Reflect.defineMetadata(
+        DECORATORS.API_RESPONSE,
+        {
+          ...responses,
+          ...groupedMetadata
+        },
+        descriptor.value
+      );
+      return descriptor;
     }
-  }
-});
-
-export const toResponseWithDefinition = (response, name) => ({
-  ...response,
-  schema: {
-    $ref: `#/definitions/${name}`
-  }
-});
+    const responses =
+      Reflect.getMetadata(DECORATORS.API_RESPONSE, target) || {};
+    Reflect.defineMetadata(
+      DECORATORS.API_RESPONSE,
+      {
+        ...responses,
+        ...groupedMetadata
+      },
+      target
+    );
+    return target;
+  };
+};
